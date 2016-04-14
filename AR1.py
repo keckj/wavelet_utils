3fd06475043e4d569bbce3e148ff605a3f428f9e
Keck Jean-Baptiste <jbkeck@hotmail.com>
2016-04-14 11:52:44 +0200

    added multidimentional support for AR1 series.

--- AR1.py
+++ AR1.py
@@ -1,8 +1,8 @@
 
 import numpy as np
 import scipy as sp
-from scipy import stats, optimize
 import matplotlib.pyplot as plt
+from scipy import stats, optimize
 
 def argmin(f,x0,tol=1.0e-8,maxiter=100,args=(),fprime=None,fprime2=None):
     return sp.optimize.newton(f,x0,fprime,args,tol,maxiter,fprime2)
@@ -16,30 +16,54 @@ def chi2inv(p,dof,tol=1.0e-8):
     x0 = 3.0
     return invert(f,p,x0,tol)
 
+# Find x_p in X such that P(X<x_p) ~= p
+def inv_cdf(X,p):
+    assert p>=0.0 and p<=1.0
+    X = np.sort(np.ravel(X))
+    return X[int(np.round(p*(X.size-1)))]
 
-def gen_AR1(m,alpha,N,sigma_e=1.0):
 
-    assert alpha>=0.0 and alpha<=1.0
+def gen_AR1(alpha,N,M=1,m=0.0,sigma_e=1.0):
+    
+    def format_input(var):
+        if np.isscalar(var):
+            var = np.asarray([var])
+        return var
 
-    En = np.random.randn(N)*sigma_e
-    AR1 = np.zeros((N), dtype=float)
+    m       = format_input(m)
+    alpha   = format_input(alpha)
+    sigma_e = format_input(sigma_e)
+    M = np.max([alpha.size, m.size, sigma_e.size])
 
-    AR1[0] = m + En[0]
+    assert m.ndim == 1 and alpha.ndim ==1 and sigma_e.ndim==1
+    assert (alpha>=0.0).all() and (alpha<=1.0).all()
+    assert (sigma_e>=0.0).all()
+    
+    shape = (M,N)
+    En = np.random.randn(*shape)*sigma_e
+    AR1 = np.zeros(shape, dtype=float)
+
+    AR1[:,0] = m[:] + En[:,0]
     for k in xrange(1,N):
-        AR1[k] = m + alpha*AR1[k-1] + En[k]
+        AR1[:,k] = m[:] + alpha[:]*AR1[:,k-1] + En[:,k]
 
     return AR1
 
 def estimate_AR1_parameters(X):
-    assert X.ndim==1, 'X should be a vector!'
-
-    N = X.size
     
-    X = X - np.mean(X)
+    assert X.ndim in [1,2], 'Wrong shape in input!'
+
+    if X.ndim==1:
+        X = X[np.newaxis,:]
+    M = X.shape[0]
+    N = X.shape[1]
+    
+    # subtract mean
+    X = X - np.mean(X,axis=1)[:,None]
 
     # AR0 and AR1 covariances
-    C0 = X.transpose().dot(X) / N                  
-    C1 = X[0:N-1].transpose().dot(X[1:N]) / (N-1) 
+    C0 = np.sum(X*X                , axis=1) / N                  
+    C1 = np.sum(X[:,0:N-1]*X[:,1:N], axis=1) / (N-1) 
      
     #A. Grinsteds
     A = C0* N**2
@@ -47,7 +71,7 @@ def estimate_AR1_parameters(X):
     C = (C0-C1)*N + C1*N**2
     delta = B**2 - 4*A*C
 
-    if delta > 0:
+    if (delta > 0.0).all():
         a = (-B-delta**0.5)/(2.0*A)
         alpha=a
     else:
@@ -56,8 +80,13 @@ def estimate_AR1_parameters(X):
     mu2 = -1.0/N + (2.0/N**2)*((N-a**N)/(1.0-a) - a*(1.0-a**(N-1)) / (1.0-a)**2)
     C0e = C0 / (1.0 - mu2)
     sigma_e = np.sqrt((1.0-a**2)*C0e)
+    
+    # remove additional dim if just one AR1 serie is estimated
+    alpha   = np.squeeze(alpha  )
+    mu2     = np.squeeze(mu2    )
+    sigma_e = np.squeeze(sigma_e)
 
-    return a,mu2,sigma_e
+    return alpha,mu2,sigma_e
 
 # Wavelet power significant level
 def WP_significant_level(wave,p):
@@ -65,6 +94,29 @@ def WP_significant_level(wave,p):
         return 0.5*chi2inv(p,dof=2)
     else:
         return 0.5*chi2inv(p,dof=1)
+
+
+#def WPC_significant_level(t,scales,x1,x2,M=100):
+    #assert x1.size == x2.size
+    #N = x1.size
+
+    #alpha1,mu1,sigma1 = estimate_AR1_parameters(x1)
+    #alpha2,mu2,sigma2 = estimate_AR1_parameters(x2)
+    
+    #X1 = gen_AR1(alpha1,N,M)
+    #X2 = gen_AR1(alpha2,N,M)
+
+    #CWT1 = wave.WT(X1)
+    #CWT2 = wave.WT(X1)
+
+    #...
+
+
+
+
+     
+
+
 
 def _Pk(alpha,Wk,dt):
     return (1.0-alpha**2.0) / (1.0+alpha**2.0 -2.0*alpha*np.cos(Wk*dt))
@@ -80,22 +132,26 @@ def _Ps(scales,wave,alpha,Wk,dt):
     return np.mean(Pk[None,:]*Psi, axis=1)
 
 if __name__ == '__main__':
-    m = 1.0
-    a = 0.88
-    N=10000
-    se=1.5
-    X = gen_AR1(m,a,N,se)
 
-    print np.mean(X),'~=', m/(1.0-a)
+    N=10000  #length of each serie
+    M=10     #number of series
+    m = 1.0                          #means
+    a = 0.50 + np.random.rand(M)/2.0 #alphas
+    se=1.5                           #sigma_es
     
+    # generate AR1 series
+    X = gen_AR1(m=m,alpha=a,N=N,sigma_e=se)
+
+    # estimate parameters
     alpha,mu2,sigma_e = estimate_AR1_parameters(X)
-
-    print 'alpha=',alpha,'~=',a
-    print 'sigma_e',sigma_e,'~=',se
     
-    dt=0.1
-    W = 2.0*np.pi*np.fft.fftfreq(N)/dt
+    # compare
+    print 'means=',np.round(np.mean(X,axis=1),2),'~=', np.round(m/(1.0-a),2)
+    print 'alpha=',np.round(alpha,3),'~=',np.round(a,3)
+    print 'sigma_e',np.round(sigma_e,3),'~=',np.round(se,3)
     
+    #dt=0.1
+    #W = 2.0*np.pi*np.fft.fftfreq(N)/dt
     #plt.figure()
     #for alpha in np.linspace(0.0,0.8,5):
         #P = _Pk(alpha,W,dt)
